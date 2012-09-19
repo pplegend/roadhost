13af91f5bfd26a7383f78a18c476d013c39a4f74
pplegend <4.m@live.cn>
2012-09-19 23:20:38 +0300

    change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
  
   private
   def initialize_nav
+     if Category.all
      @categories=Category.all
+    else
+     @categories=['semi','whell']
+    end
   end
 end
